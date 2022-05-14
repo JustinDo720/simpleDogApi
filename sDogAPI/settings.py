3639e25b905e5c5f67fc7244b66d9090c27d00bf
Your Name <justindo720@gmail.com>
2022-05-14 02:45:16 -0400

    Changed bucket

--- sDogAPI/settings.py
+++ sDogAPI/settings.py
@@ -153,13 +153,15 @@ REST_FRAMEWORK = {
 
 # AWS config
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_DEFAULT_ACL = None
 AWS_S3_FILE_OVERWRITE = False
 
 AWS_S3_ACCESS_KEY_ID = os.getenv('POWER_PET_PRO_S3_2_ACCESS_KEY')
 AWS_S3_SECRET_ACCESS_KEY = os.getenv('POWER_PET_PRO_S3_2_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'profile-api'
+AWS_STORAGE_BUCKET_NAME = 'dog-api'
 
-AWS_S3_REGION_NAME = 'us-east-1'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'us-east-2'
 
 django_heroku.settings(locals())
