00cc79fd8b569834cc5331858f01900f947a2825
jd <justindo720@gmail.com>
2022-05-14 11:37:23 -0400

    removed shared_date

--- sDogAPIApp/models.py
+++ sDogAPIApp/models.py
@@ -6,7 +6,9 @@ class Dog(models.Model):
     dog_name = models.CharField(max_length=100)
     photo = models.ImageField(upload_to='dog_img/', blank=True, null=True)
     owner_name = models.CharField(max_length=100)
-    shared_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.dog_name
+        if self.dog_name:
+            return self.dog_name
+        else:
+            return '?'
